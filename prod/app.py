27746e5c575e773bc66ab3eda1cc14c86bc8a757
dailywoes <johnrteixeira@gmail.com>
2020-12-11 04:13:11 -0500

    first push

--- prod/app.py
+++ prod/app.py
@@ -1,6 +1,17 @@
 import json
 import boto3
-import mysql.connector
+import rds_config
+import pymysql
+
+rds_host = 'imdbdataset.cylq1mw66qwl.ca-central-1.rds.amazonaws.com'
+username = rds_config.masterUsername
+password = rds_config.masterPassword
+database = rds_config.imdbdataset
+
+try:
+    con = pymysql.connect(rds_host, user=username, passwd=password, db=database, connect_timeout=5)
+except pymysql.MySQLError as e:
+    print('whoops')
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb',region_name='ca-central-1')
@@ -19,6 +30,11 @@ def lambda_handler(event, context):
             ':val1': item['Item']['quantity'] + 1
         }
     )
+    rds = boto3.resource('rds')
+    #input_title = event['title']
+    #input_year = event['year']
+    #event["queryStringParameters"]['title']
+    #print(input_title)
     return {
         "statusCode": 200,
         "headers": {
@@ -26,5 +42,5 @@ def lambda_handler(event, context):
         },
         #Lambda thinks the entry is a decimal for some reason, cast
         #the value to an integer since json doesnt accept decimal
-        "body": json.dumps(int(item['Item']['quantity']))
+        "body": json.dumps(event["queryStringParameters"]['title'])
     }
