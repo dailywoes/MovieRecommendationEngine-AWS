793f0a3cb23f46f35e3dc3fdc1c60f66c8a780ef
dailywoes <johnrteixeira@gmail.com>
2020-12-11 02:30:55 -0500

    first push

--- prod/app.py
+++ prod/app.py
@@ -1,5 +1,7 @@
 import json
 import boto3
+import mysql.connector
+
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb',region_name='ca-central-1')
     table = dynamodb.Table('webpage')
