302683062ecb4b4ca125bf2c6ed6638875b55bb5
dailywoes <johnrteixeira@gmail.com>
2020-12-12 16:25:07 -0500

    pushing suggestion code

--- prod/app.py
+++ prod/app.py
@@ -9,8 +9,28 @@ def lambda_handler(event, context):
     userpass = 'masterPassword'
     con = pymysql.connect(host=rds_host,user=useruser,password=userpass,port=3306,database='imdbdataset')
     imdb_movie = pd.read_sql_query('select * from imdb_movie_clean_new;', con)
-    print(imdb_movie)
-    print(test)
+    imdb_movie_map = pd.read_sql_query('select * from imdb_movie_map;', con, index_col=['index'])
+
+    input_title = 'Iron Man'
+    input_year = 2008
+
+    input_coord = imdb_movie_map[(imdb_movie['imdb_title'].str.contains('Iron Man')
+                                              & (imdb_movie['imdb_year'] == 2008))]
+
+    for column, contents in imdb_movie_map.items():
+        imdb_movie_map[column] = imdb_movie_map[column].subtract(input_coord.iloc[:, imdb_movie_map.columns.
+                                                     get_loc(column)].values[0], fill_value=0)
+        imdb_movie_map[column] = imdb_movie_map[column].pow(2)
+    imdb_movie_map['sum'] = imdb_movie_map.sum(axis=1, skipna=True)
+    imdb_movie_map['dist'] = imdb_movie_map['sum'].pow(1. / 2)
+    imdb_movie_map['rating'] = imdb_movie['imdb_rating'].multiply(-1)
+
+    result = imdb_movie_map.nsmallest(25, ['dist'])
+    result = imdb_movie.loc[result.nsmallest(10, ['rating']).index]
+    result = result['imdb_title']
+
+    print(result)
+    
     dynamodb = boto3.resource('dynamodb',region_name='ca-central-1')
     table = dynamodb.Table('webpage')
     item = table.get_item(
